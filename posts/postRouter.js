fca161b00259c844ec811d0e869cecc3b26c1acb
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-08-13 18:15:18 -0500

    server running

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,19 @@
-const express = 'express';
-
+const express = require('express');
+const post = require('./postDb.js')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    Hubs.find(req.query)
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the hubs',
+      });
+    });
 });
 
 router.get('/:id', (req, res) => {
