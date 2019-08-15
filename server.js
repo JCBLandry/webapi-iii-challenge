9d8ca9a181e25a64edd756bca69d397f23e265ea
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-08-15 18:26:17 -0500

    forgot logger

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userRouter = require('./users/userRouter.js')
+const morgan = require('morgan');
 const server = express();
 
+server.use(morgan('dev'));
+server.use(logger());
 server.use(express.json())
 server.use('/api/users', userRouter)
 
@@ -12,7 +15,12 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+  const tod = new Date().toLocaleTimeString()
+  
+  const date = new Date().toLocaleDateString()
 
+  console.log(`${req.method} | http://localhost:4000${req.url} | ${tod} , ${date}`)
+  next()
 };
 
 module.exports = server;
